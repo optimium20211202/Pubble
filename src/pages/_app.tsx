7430b714bd7f99aadd3fe17bd8b6fc2eca93c8d4
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2023-06-17 20:53:02 +0900

    iOS実機だとstyle崩れる...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       https://stackoverflow.com/questions/24193272/overflow-xhidden-on-mobile-device-not-working */}
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0 viewport-fit=cover"
         />
       </Head>
       <RecoilRoot>
